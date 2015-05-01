7f0d47fcc51aeca06ecfb2eb7f08b4985598edc4
Greg Finak <greg.finak@gmail.com>
2015-05-01 12:44:06 -0700

    Add RGLab_report template

--- R/rglab_report.R
+++ R/rglab_report.R
@@ -1,6 +1,6 @@
 #'@export
-visc_report <- function(latex_engine = "pdflatex") {
-  template <- find_resource("visc_report", "template.tex")
+RGLab_report <- function(latex_engine = "pdflatex") {
+  template <- find_resource("RGLab_report", "template.tex")
 
   rmarkdown::pdf_document(
     template = template, keep_tex=TRUE, fig_caption = TRUE, latex_engine=latex_engine)
