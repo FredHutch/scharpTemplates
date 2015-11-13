15c7b1c817500afc6011bee3c3119f270868c94b
pobrecht <pobrecht@scharp.org>
2015-11-13 15:55:56 -0800

    removed visc_sap template

--- R/visc_sap.R
+++
@@ -1,9 +0,0 @@
-#'@export
-visc_sap <- function(latex_engine = "pdflatex") {
-  template <- find_resource("visc_sap", "template.tex")
-
-  rmarkdown::pdf_document(
-    template = template, keep_tex=TRUE, fig_caption = TRUE, latex_engine=latex_engine)
-
-}
-
