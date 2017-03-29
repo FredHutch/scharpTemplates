53855624fe6be60b4b1ba10b59fec1bd54345347
pobrecht <pobrecht@scharp.org>
2017-03-29 12:15:17 -0700

    Fixed render2x() function.

--- inst/rmarkdown/templates/visc_report/skeleton/render2x.R
+++
@@ -1,2 +0,0 @@
-  rmarkdown::render(file.path("mypath", "myRmd.Rmd"), output_format = 'word_document', output_options=list(fig_caption = TRUE, fig_height = 5, fig_width = 5))
-  rmarkdown::render(file.path("mypath", "myRmd.Rmd"))
