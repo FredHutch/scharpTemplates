a2e76ab208f056447c19857675b01e49347c7c71
wfulp <wfulp@fredhutch.org>
2019-05-13 13:13:00 -0700

    Adding keep_tex as a param in YAML

--- R/visc_report.R
+++ R/visc_report.R
@@ -1,13 +1,13 @@
 #'@export
-visc_report <- function(latex_engine = "pdflatex") {
+visc_report <- function(latex_engine = "pdflatex", keep_tex = TRUE) {
   template <- find_resource("visc_report", "template.tex")
 
   rmarkdown::pdf_document(
-    template = template, keep_tex=TRUE, fig_caption = TRUE, latex_engine=latex_engine)
+    template = template, keep_tex = keep_tex, fig_caption = TRUE, latex_engine = latex_engine)
 
 }
 
 render2x <- function(path, name){
-  rmarkdown::render(file.path(path, name), envir=.GlobalEnv, output_format = 'word_document', output_options=list(fig_caption = TRUE, fig_height = 5, fig_width = 5))
-  rmarkdown::render(file.path(path, name), envir=.GlobalEnv)
+  rmarkdown::render(file.path(path, name), envir = .GlobalEnv, output_format = 'word_document', output_options = list(fig_caption = TRUE, fig_height = 5, fig_width = 5))
+  rmarkdown::render(file.path(path, name), envir = .GlobalEnv)
 }
