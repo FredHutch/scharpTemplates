15025631848696dbca5fcaf89a67b8b3171ff663
wfulp <wfulp@fredhutch.org>
2019-06-07 14:59:41 -0700

    multi template now refers to proper visc_report_multi tex file, along with fixing fancyhdr warning

--- R/visc_report.R
+++ R/visc_report.R
@@ -7,6 +7,16 @@ visc_report <- function(latex_engine = "pdflatex", keep_tex = TRUE) {
 
 }
 
+#'@export
+visc_report_multi <- function(latex_engine = "pdflatex", keep_tex = TRUE) {
+  template <- find_resource("visc_report_multi", "template.tex")
+
+  rmarkdown::pdf_document(
+    template = template, keep_tex = keep_tex, fig_caption = TRUE, latex_engine = latex_engine)
+
+}
+
+
 render2x <- function(path, name){
   rmarkdown::render(file.path(path, name), envir = .GlobalEnv, output_format = 'word_document', output_options = list(fig_caption = TRUE, fig_height = 5, fig_width = 5))
   rmarkdown::render(file.path(path, name), envir = .GlobalEnv)
